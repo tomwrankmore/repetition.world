e41fe5debc7b3c93d6dd5b584163619819f5e0ad
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2026-03-18 09:11:14 +0000

    publish

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <div>
-      <h1 className="text-center md:text-left text-xl md:text-xl mb-12 w-full md:w-[40rem] tracking-wide">Tom / Eliphino is a London based music producer, DJ &amp; composer.</h1>
+      <h1 className="text-center md:text-left text-base mb-12 w-full md:w-[40rem] tracking-wide">I am a London based music producer, DJ &amp; composer.</h1>
       
       <ul className="w-full md:w-fit">
         {projects.map((project) => {
