4cde8ef7c0d3ea38687536a101630c074cd3b3a7
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2026-05-22 14:40:52 +0100

    icons

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { FaSpotify, FaSoundcloud, FaBandcamp } from 'react-icons/fa';
+
 const today = new Date();
 const yyyy = today.getFullYear();
 
@@ -5,7 +7,6 @@ const Footer = () => {
   return (
     <footer>
       <p className="text-xs text-center">
-        contact:{" "}
         <a
           href="mailto:eliphinomusic@gmail.com"
           target="_blank"
@@ -15,6 +16,19 @@ const Footer = () => {
           eliphinomusic@gmail.com
         </a>
       </p>
+
+      <div className="flex justify-center gap-4 my-3">
+        <a href="https://soundcloud.com/eliphino" target="_blank" rel="noopener noreferrer">
+          <FaSoundcloud className="text-lg" />
+        </a>
+        <a href="https://eliphino.bandcamp.com/music" target="_blank" rel="noopener noreferrer">
+          <FaBandcamp className="text-lg" />
+        </a>
+        <a href="https://open.spotify.com/artist/7xqnFwk6ZueWJ1IA8gsEWM" target="_blank" rel="noopener noreferrer">
+          <FaSpotify className="text-lg" />
+        </a>
+      </div>
+
       <p className="text-xs text-center">&copy; {yyyy}</p>
     </footer>
   );
