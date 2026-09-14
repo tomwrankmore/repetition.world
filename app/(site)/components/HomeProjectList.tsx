a4d64fd2dc72531acd6951fc8b6ef0ef4f29510c
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2026-09-14 17:06:40 +0100

    images for projects

--- app/(site)/components/HomeProjectList.tsx
+++ app/(site)/components/HomeProjectList.tsx
@@ -4,13 +4,16 @@ import { useRef } from "react";
 import Link from "next/link";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import Image from "next/image";
 
 gsap.registerPlugin(useGSAP);
 
 type Project = {
   _id: string;
+  title?: string;
   name?: string;
   slug: string;
+  image?: string;
 };
 
 export default function HomeProjectList({ projects }: { projects: Project[] }) {
@@ -30,14 +33,23 @@ export default function HomeProjectList({ projects }: { projects: Project[] }) {
   // );
 
   return (
-    <ul ref={listRef} className="w-full md:w-fit">
+    <ul ref={listRef} className="grid w-full grid-cols-3 gap-2">
       {projects.map((project) => (
-        <li key={project._id} className="mb-2">
+        <li key={project._id}>
           <Link
             href={`/projects/${project.slug}`}
-            className="text-xs block md:whitespace-nowrap hover:underline"
+            className="group relative block overflow-hidden text-xs break-words"
           >
-            {project?.name}
+            <Image
+              src={project.image || "/default-image.jpg"}
+              alt={project.name || "Project Image"}
+              width={960}
+              height={600}
+              className="aspect-video h-full w-full object-cover transition-transform duration-500 ease-out group-hover:scale-105"
+            />
+            <span className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black/70 px-2 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 ">
+              {project.name || "Untitled project"}
+            </span>
           </Link>
         </li>
       ))}
